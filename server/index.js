78b06f7b24fed17eb717642eb740ec65ce548dbf
Jacob Ankney <crosswalkastronaut@Jacobs-MacBook-Pro-2.local>
2019-10-29 17:56:43 -0600

    massive syntax in

--- server/index.js
+++ server/index.js
@@ -1,10 +1,30 @@
 const express=require('express')
-
 const app=express()
+require('dotenv').config()
 const {PORT, DATABASE_STRING}=process.env
-console.log(PORT)
+const massive=require('massive')
+
+//require controllers here
+
+app.use(express.json())
+
+app.use(express.static(`${__dirname}/../build`));
+
+//If this syntax does not work, try the second syntax
+// massive(DATABASE_STRING).then((db)=>{
+    // app.set('db', db)
+    //     console.log('db connected')
+// })).catch(err=>console.log(err, 'database connection err'))
+
+// massive({ connectionString: process.env.DATABASE_URL, 
+//     ssl: true 
+//   }).then(database => {
+//       app.set("db", database);
+//       console.log("db connected");
+//     }).catch(err => console.log("database error", err));
+
 
 
 app.listen(PORT, ()=>{
-    console.log('listening on ', process.env.PORT)
+    console.log('listening on ', PORT)
 })
